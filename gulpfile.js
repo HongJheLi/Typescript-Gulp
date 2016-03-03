f586709d4c1b1d464dd000be3dcc5d5fafcb5ba2
Hong-Jhe <hongjhe@kfsyscc.org>
2016-03-03 17:32:11 +0800

    Added browser-sync.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,24 @@
 var gulp = require('gulp');
 var tsc = require('gulp-tsc');
 var exec = require('child_process').exec;
+var browserSync = require('browser-sync');
+
+gulp.task('browser-sync', function() {
+  browserSync.init(null, {
+    server: {
+      baseDir: "./"
+    }
+  });
+});
 
 gulp.task('tsc-compile', function(){
   gulp.src(['src/**/*.ts'])
     .pipe(tsc())
     .pipe(gulp.dest('dest/'))
+    .pipe(browserSync.reload({stream:true}));
 });
 
-gulp.task('tsc-watch', function () {
+gulp.task('tsc-watch', ["tsc-compile", "browser-sync"], function () {
   gulp.watch('src/**/*.ts', ['tsc-compile']);
 });
 
